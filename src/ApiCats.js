6d69d34b41459cdd51a22bf8cfe8532de73f0513
dreinasant <ao.andreina@gmail.com>
2022-06-13 19:18:50 -0300

    atualização

--- src/ApiCats.js
+++ src/ApiCats.js
@@ -4,9 +4,11 @@ import axios from "axios";
 
 export default function API() {
   const [dog, setDog] = useState([]);
+  const [aparecerImagem, setAparecerImagens] = useState (false)
   function DogList() {
     axios.get(`https://dog.ceo/api/breeds/image/random`).then((response) => {
       setDog(response.data.message);
+      setAparecerImagens(true)
     });
   }
 
@@ -22,7 +24,9 @@ export default function API() {
       </button>
       </div>
       <div className="box-img">
-      <img className="imagens-dogs" src={dog} alt="fotos-de-doguinhos" />
+        <ul>
+      <li>{ aparecerImagem && <img className="imagens-dogs" src={dog} alt="fotos-de-doguinhos" /> }</li>
+      </ul>
       </div>
     </div>
   );
